8c5766bd670fbcd25d9fb062c9f28140e43df86f
jgblanc <jgblanc@uchicago.edu>
2023-07-28 14:06:51 -0500

    t

--- code/hgdp/get_pairwise_EUR_AF_contrasts.R
+++ code/hgdp/get_pairwise_EUR_AF_contrasts.R
@@ -13,10 +13,12 @@ suppressWarnings(suppressMessages({
 
 spnum = as.numeric(args[1])
 out_pre = args[2+spnum]
+master_filepath = args[3+spnum]
 
 # Get all pairwise combos
 pw_comb <- t(combn(seq(1, spnum),2))
 
+df <- pw_comb
 for (i in 1:nrow(pw_comb)) {
 
   # Get pairwise IDs
@@ -39,6 +41,10 @@ for (i in 1:nrow(pw_comb)) {
   nm1 <- strsplit(strsplit(args[id1+1], "/")[[1]][11], "_")[[1]][1]
   nm2 <- strsplit(strsplit(args[id2+1], "/")[[1]][11], "_")[[1]][1]
 
+  # Change in output
+  df[i,1] <- nm1
+  df[i,2] <- nm2
+
   # Get chromosome number
   chr <- strsplit(strsplit(strsplit(args[id1+1], "/")[[1]][11], "_")[[1]][2], ".afreq")[[1]][1]
 
@@ -49,6 +55,9 @@ for (i in 1:nrow(pw_comb)) {
   fwrite(out, outfile,row.names = F, col.names = T, quote = F, sep = "\t")
 
 }
+
+# Save master file
+fwrite(df, master_filepath ,row.names = F, col.names = T, quote = F, sep = "\t")
 
 
 
