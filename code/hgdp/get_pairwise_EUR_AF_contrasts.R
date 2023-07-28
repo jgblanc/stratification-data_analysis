7c16de0bed1d44a9bacea8a0effdaf7232756ef5
jgblanc <jgblanc@uchicago.edu>
2023-07-28 13:39:32 -0500

    t

--- code/hgdp/get_pairwise_EUR_AF_contrasts.R
+++ code/hgdp/get_pairwise_EUR_AF_contrasts.R
@@ -12,13 +12,43 @@ suppressWarnings(suppressMessages({
 }))
 
 spnum = as.numeric(args[1])
-print(spnum)
+out_pre = args[2+spnum]
 
-for (i in 2:(spnum+1)) {
+# Get all pairwise combos
+pw_comb <- t(combn(seq(1, spnum),2))
 
-  print(args[i])
+for (i in 1:nrow(pw_comb)) {
+
+  # Get pairwise IDs
+  id1 <- pw_comb[i,1]
+  id2 <- pw_comb[i,2]
+
+  # Read in dataframes
+  df1 <- fread(args[id1+1])
+  df2 <- fread(args[id2+1])
+
+  # Compute r
+  r <- df1$ALT_FREQS - df1$ALT_FREQS
+
+  # Set up output dataframe
+  out <- df1[,1:4]
+  out$r <- r
+
+  # Get subpop names
+  nm1 <- strsplit(strsplit(args[id1+1], "/")[[1]][5], "_")[[1]][1]
+  nm2 <- strsplit(strsplit(args[id2+1], "/")[[1]][5], "_")[[1]][1]
+
+  # Get chromosome number
+  chr <- strsplit(strsplit(strsplit(x, "/")[[1]][5], "_")[[1]][2], ".afreq")[[1]][1]
+
+
+  # Save output
+  outfile <- paste0(out_pre, nm1, "-", nm2, "_", chr, ".rvec")
+  print(outfile)
+  fwrite(df, outfile,row.names = F, col.names = T, quote = F, sep = "\t")
 
 }
+
 
 
 
