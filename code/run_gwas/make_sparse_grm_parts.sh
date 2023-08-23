f7c1211a46b10714222a4444948035fc77762009
jgblanc <jgblanc@uchicago.edu>
2023-08-23 18:37:00 -0500

    need to parallelize

--- code/run_gwas/make_sparse_grm_parts.sh
+++ code/run_gwas/make_sparse_grm_parts.sh
@@ -5,7 +5,8 @@ snpList=$2
 nparts=$3
 outPrfx=$4
 
-for i in {1..$nparts}
+#for i in $( seq 2 $nparts )
+for i in {3..3}
 do
     echo $i
     ./bin/gcta64 --mpfile $chrList --extract $snpList --make-grm-part $nparts $i --thread-num 10 --out $outPrfx
