a5a4abc9d6692a4cb08a1647fa0f7c94095ee38b
jgblanc <jgblanc@uchicago.edu>
2023-08-25 10:17:32 -0500

    t

--- code/run_gwas/make_sparse_grm_parts.sh
+++ code/run_gwas/make_sparse_grm_parts.sh
@@ -6,4 +6,4 @@ nparts=$3
 indx=$4
 outPrfx=$5
 
-./bin/gcta64 --mpfile $chrList --extract $snpList --make-grm-part $nparts $indx --thread-num 10 --out $outPrfx
+./bin/gcta64 --mpfile $chrList --extract $snpList --make-grm-part $nparts $indx --thread-num 20 --out $outPrfx
