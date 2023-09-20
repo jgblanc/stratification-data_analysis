901d8de36d0342457bacf408f1410e082673efd1
jgblanc <jgblanc@uchicago.edu>
2023-09-20 16:01:25 -0500

    t

---
+++ code/prs/format_ss_for_plink.R
@@ -0,0 +1,25 @@
+# Script to readjust SNPs IDs to only include CHR:POS
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Provide <vilma format> <output>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(tidyverse)
+}))
+
+ss=args[1]
+outfile = args[2]
+
+
+# Read in first data frame
+df <- fread(ss)
+
+# Fix IDs
+df <- df %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
+  select(ID,A1, posterior_ukbb)
+
+# Save output
+fwrite(df, outfile,col.names=T,row.names=F,quote=F,sep="\t")
+
