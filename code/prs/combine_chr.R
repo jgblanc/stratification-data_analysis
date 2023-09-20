127f1b8b3b83aa1b36fc4209cc1c6c9513d1ec71
jgblanc <jgblanc@uchicago.edu>
2023-09-20 15:07:24 -0500

    t

--- code/prs/combine_chr.R
+++ code/prs/combine_chr.R
@@ -2,20 +2,49 @@
 
 args=commandArgs(TRUE)
 
-if(length(args)<4){stop("Provide <list of chromosome numbers> <first prefix> <second prefix> <output>")}
+if(length(args)<5){stop("Provide <number of total chrosomesome> <first prefix> <second prefix> <output> <list of chromosome numbers> ")}
 
 suppressWarnings(suppressMessages({
   library(data.table)
   library(tidyverse)
 }))
 
-chrlist = args[1]
+chr_num = as.numeric(args[1])
 p1 = args[2]
 p2 = args[3]
 outfile = args[4]
 
-print(chrlist)
+
+print(chr_num)
 print(p1)
 print(p2)
 
+# Read in first data frame
+fn <- paste0(p1, "_", args[5], p2)
+print(fn)
+df <- fread(fn)
+
+# Fix IDs
+df <- df %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
+  select(ID,A1, A2, posterior_ukbb, posterior_variance_ukbb)
+
+
+for (i in 2:chr_num) {
+
+  # Read in data frame
+  fn <- paste0(p1, "_", args[4+i], p2)
+  print(fn)
+  tmp <- fread(fn)
+
+  # Fix IDs
+  tmp <- tmp %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
+    select(ID,A1, A2, posterior_ukbb, posterior_variance_ukbb)
+
+  # Add to existing df
+  df <- rbind(df, tmp)
+
+}
+
+# Save output
+fwrite(df, out_file,col.names=T,row.names=F,quote=F,sep="\t")
 
