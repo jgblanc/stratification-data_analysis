d22fb74576b69162ae64d59c84e3c44dd60cbef3
jgblanc <jgblanc@uchicago.edu>
2023-09-20 16:24:22 -0500

    ready to make pgs

--- code/prs/combine_chr.R
+++ code/prs/combine_chr.R
@@ -24,26 +24,27 @@ fn <- paste0(p1, "_", args[5], p2)
 print(fn)
 df <- fread(fn)
 
-# Fix IDs
-df <- df %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
-  select(ID,A1, A2, posterior_ukbb, posterior_variance_ukbb)
 
+if (chr_num > 1) {
 
-for (i in 2:chr_num) {
+  for (i in 2:chr_num) {
 
-  # Read in data frame
-  fn <- paste0(p1, "_", args[4+i], p2)
-  print(fn)
-  tmp <- fread(fn)
+    print(paste0("Adding chr num ", args[4+i]))
 
-  # Fix IDs
-  tmp <- tmp %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
-    select(ID,A1, A2, posterior_ukbb, posterior_variance_ukbb)
+    # Read in data frame
+    fn <- paste0(p1, "_", args[4+i], p2)
+    print(fn)
+    tmp <- fread(fn)
 
-  # Add to existing df
-  df <- rbind(df, tmp)
+    # Add to existing df
+    df$NAMED_ALLELE_DOSAGE_SUM <- df$NAMED_ALLELE_DOSAGE_SUM + tmp$NAMED_ALLELE_DOSAGE_SUM
+    df$SCORE1_SUM <- df$SCORE1_SUM + tmp$SCORE1_SUM
+
+  }
 
 }
+
+
 
 # Save output
 fwrite(df, outfile,col.names=T,row.names=F,quote=F,sep="\t")
