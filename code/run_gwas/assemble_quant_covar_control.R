3724b0746e153ec897799814854211dbd071b5af
jgblanc <jgblanc@uchicago.edu>
2023-09-21 16:18:38 -0500

    need to add control

---
+++ code/run_gwas/assemble_quant_covar_control.R
@@ -0,0 +1,33 @@
+# Script to format quantitative covariates when not including FGr
+
+args=commandArgs(TRUE)
+
+if(length(args)<5){stop("Provide path to covariate files")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(tidyverse)
+}))
+
+age = args[1]
+pc = args[2]
+pcNum = as.numeric(args[3])
+outfile = args[4]
+
+# Read in dataframes
+dfAge <- fread(age)
+dfPc <- fread(pc)
+
+# Subset to correct number of PCs
+dfPc <- dfPc[,1:(pcNum + 2)]
+print(ncol(dfPc))
+
+# Join files
+df <- inner_join(dfAge, dfPc, by = c("#FID", "IID"))
+
+# Drop rows with NA
+df <- drop_na(df)
+
+# Save file
+fwrite(df, outfile,col.names=T,row.names=F,quote=F,sep="\t")
+
