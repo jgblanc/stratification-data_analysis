ad9e58ff67ec7879c53228ac36446e86a7bc848f
jgblanc <jgblanc@uchicago.edu>
2023-10-12 12:32:25 -0500

    t

--- code/prs/format_ss_for_plink.R
+++ code/prs/format_ss_for_plink.R
@@ -17,8 +17,13 @@ outfile = args[2]
 df <- fread(ss)
 
 # Fix IDs
+#df <- df %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
+#  select(ID,A1, posterior_ukbb)
+
+colnames(df) <- c("#CHROM", "ID", "POS", "REF", "ALT", "r", "posterior_ukbb","block")
 df <- df %>% separate(ID, c("ID", "tmp1", "tmp2"), "_") %>%
-  select(ID,A1, posterior_ukbb)
+  selec(ID, ALT, posterior_ukbb)
+
 
 # Save output
 fwrite(df, outfile,col.names=T,row.names=F,quote=F,sep="\t")
