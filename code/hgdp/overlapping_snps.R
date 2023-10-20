ecda413b422a82923ff5f8797d42533b7e5ee527
jgblanc <jgblanc@uchicago.edu>
2023-10-20 15:40:05 -0500

    t

--- code/hgdp/overlapping_snps.R
+++ code/hgdp/overlapping_snps.R
@@ -22,21 +22,8 @@ tp <- fread(tp_file)
 ukbb <- subset(ukbb, ukbb$ALT_FREQS > 0.01 & ukbb$ALT_FREQS < 0.99)
 tp <- subset(tp, tp$ALT_FREQS > 0.01  & tp$ALT_FREQS < 0.99)
 
-# Subset to SNPs with missingness rate < 5%
-tp <- subset(tp, tp$OBS_CT > (0.95 * max(tp$OBS_CT)))
-ukbb <- subset(ukbb, ukbb$OBS_CT > (0.95 *  max(ukbb$OBS_CT)))
-
 # Get overlapping SNPs with same alt/ref
 matched <- inner_join(ukbb, tp, by = c("#CHROM", "ID", "ALT", "REF"))
-matched <- matched %>% select(`#CHROM`,ID,REF,ALT )
 
-# Get overlapping SNPs with opposite alt/ref
-tmp1 <- inner_join(ukbb, tp, by = c("#CHROM", "ID"))
-flipped <- subset(tmp1, tmp1$REF.x == tmp1$ALT.y & tmp1$ALT.x == tmp1$REF.y)
-flipped <- flipped %>% select("#CHROM", ID, REF.x, REF.y)
-colnames(flipped) <- c("#CHROM", "ID", "REF", "ALT")
-
-# Get complete list of SNPs and list the reference allele in UKBB
-out <- rbind(matched, flipped) %>% select(ID, REF)
-fwrite(out,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+fwrite(matched,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
 
