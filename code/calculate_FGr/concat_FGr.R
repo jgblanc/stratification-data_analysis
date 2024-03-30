15a72a7dc32ee1c42059887621fb4a2b352705d6
jgblanc <jgblanc@uchicago.edu>
2024-03-30 10:58:50 -0500

    t

---
+++ code/calculate_FGr/concat_FGr.R
@@ -0,0 +1,38 @@
+## Concatenate projections
+## This script reads in all the projected test vectors for each chromosome and adds them all together, leaving the focal chromosome out, and scales the final covariate
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript conccat_FGr.R <prefix to Tm chromosomes> <chromosome number> <outfile>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+outfile = args[1]
+
+# Get list of all but the focal chromosome
+chrs <- seq(1,22)
+chrs <- chrs[ !chrs == chrNum]
+
+# Add Tm for each chromosome to each other
+df <- fread(args[2])
+for (i in 3:length(args)) {
+
+  new <- fread(args[i])
+  df$FGr <- df$FGr + new$FGr
+
+}
+
+df$FGr <- scale(df$FGr)
+
+# Save output
+fwrite(df, outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+
+
+
+
+
+
+
