49438289c8fc4ad3a197a9d2c6edd2c30096910f
jgblanc <jgblanc@uchicago.edu>
2024-03-30 14:42:45 -0500

    t

--- code/PGA_paper/get_IDs_B.R
+++ code/PGA_paper/get_IDs_B.R
@@ -18,21 +18,21 @@ genotyped = args[6]
 outGWAS = args[7]
 outTest = args[8]
 gwasSizeWBS = as.numeric(args[9])
-gwasSizeWhite = as.numeric(args[10])
+gwasSizeOther = as.numeric(args[10])
 testSize = as.numeric(args[11])
 wbs= args[12]
 white = args[13]
-
+all = args[14]
 
 ## Read in all dataframes and join them
 df <- fread(sex,  colClasses = 'character')[,1:2]
 df <- inner_join(df, fread(batch,  colClasses = 'character')[,1:2])
-#df <- inner_join(df, fread(north,  colClasses = 'character')[,1:2])
-#df <- inner_join(df, fread(east,  colClasses = 'character')[,1:2])
 df <- inner_join(df, fread(age,  colClasses = 'character')[,1:2])
 df <- inner_join(df, fread(genotyped,  colClasses = 'character')[,1:2])
 df_wbs <- inner_join(df, fread(wbs,  colClasses = 'character')[,1:2])
 df_white <- inner_join(df,fread(white,  colClasses = 'character')[,1:2])
+df_all <- inner_join(df,fread(all,  colClasses = 'character')[,1:2])
+df_other <- df_all %>% filter(!IID %in% df_white$IID)
 
 ## Select test panel
 df_test <- df_wbs %>% sample_n(testSize) %>% select("#FID", "IID")
@@ -40,7 +40,7 @@ fwrite(df_test, outTest ,row.names=F,quote=F,sep="\t", col.names = T)
 
 ## Select gwas panel
 df_GWAS1 <- df_wbs %>% filter(!IID %in% df_test$IID) %>% sample_n(gwasSizeWBS) %>% select("#FID", "IID")
-df_GWAS2 <- df_white %>% filter(!IID %in% df_test$IID) %>% sample_n(gwasSizeWhite) %>% select("#FID", "IID")
+df_GWAS2 <- df_other %>% filter(!IID %in% df_test$IID) %>% sample_n(gwasSizeOther) %>% select("#FID", "IID")
 df_GWAS <- rbind(df_GWAS1, df_GWAS2)
 fwrite(df_GWAS, outGWAS ,row.names=F,quote=F,sep="\t", col.names = T)
 
