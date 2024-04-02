7943e9cbc8b8dd90c7b3cffce8e123df1a2ad250
jgblanc <jgblanc@uchicago.edu>
2024-04-01 21:51:21 -0500

    t

---
+++ code/PGA_paper/assemble_quant_covar_control.R
@@ -0,0 +1,24 @@
+# Script to format quantitative covariates when not including FGr
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Provide path to covariate files")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(tidyverse)
+}))
+
+age = args[1]
+outfile = args[2]
+
+# Read in dataframes
+dfAge <- fread(age)
+
+
+# Drop rows with NA
+df <- drop_na(df)
+
+# Save file
+fwrite(df, outfile,col.names=T,row.names=F,quote=F,sep="\t")
+
