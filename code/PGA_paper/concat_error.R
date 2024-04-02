8c6162a2069ddf0c8fd39a08d85bd9f09bd1e0b2
jgblanc <jgblanc@uchicago.edu>
2024-04-02 10:54:51 -0500

    t

---
+++ code/PGA_paper/concat_error.R
@@ -0,0 +1,49 @@
+# This script concatenates results from computing the error in FGr
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript concat_results.R <output file> <input files>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(tidyverse)
+}))
+
+
+outfile = args[1]
+
+dfOut <- matrix(NA, nrow = 1, ncol = 4)
+
+for (i in 2:length(args)) {
+
+  # Get results file name
+  filename = args[i]
+
+  # Extract parameters
+  tmp <- strsplit(filename, "/")[[1]]
+  rep <- tmp[4]
+  nsnp <- as.numeric(strsplit(tmp[5], "L-")[[1]][2])
+  contrast <- strsplit(strsplit(tmp[6], "Var_")[[1]][2], ".txt")[[1]][1]
+
+  # Read in results
+  dfTmp <- fread(filename)
+  varE <- dfTmp[1,1]
+
+  # Save results in table
+  x <- c(varE, rep, nsnp,contrast)
+  dfOut <- rbind(dfOut, x)
+
+}
+
+# Remove first row
+dfOut <- as.data.frame(dfOut[2:nrow(dfOut),])
+colnames(dfOut) <- c("error", "rep", "L",  "contrast")
+
+# Save file
+print(dfOut)
+fwrite(dfOut,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+
+
+
+
---
+++ code/PGA_paper/concat_q.R
@@ -0,0 +1,52 @@
+# This script concatenates results from computing the error in FGr
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript concat_results.R <output file> <input files>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(tidyverse)
+}))
+
+
+outfile = args[1]
+
+dfOut <- matrix(NA, nrow = 1, ncol = 6)
+
+for (i in 2:length(args)) {
+
+  # Get results file name
+  filename = args[i]
+
+  # Extract parameters
+  tmp <- strsplit(filename, "/")[[1]]
+  rep <- tmp[4]
+  nsnp <- as.numeric(strsplit(tmp[5], "L-")[[1]][2])
+  phenotype <- tmp[6]
+  covar <- strsplit(tmp[8], "_")[[1]][1]
+  contrast <- strsplit(strsplit(tmp[8], "_")[[1]][2], ".all.results.txt")[[1]][1]
+
+  # Read in results
+  dfTmp <- fread(filename)
+  q <- dfTmp[1,1]
+  pval <- dfTmp[1,2]
+
+  # Save results in table
+  x <- c(q,pval, rep, nsnp,contrast, phenotype, covar)
+  dfOut <- rbind(dfOut, x)
+
+}
+
+# Remove first row
+dfOut <- as.data.frame(dfOut[2:nrow(dfOut),])
+colnames(dfOut) <- c("q", "pval","rep", "L", "contrast", "phenotype","covar")
+
+# Save file
+print(dfOut)
+fwrite(dfOut,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+
+
+
+
