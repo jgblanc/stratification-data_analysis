5cea7bbb2e9d9a36f562e7bf6673736088f70467
jgblanc <jgblanc@uchicago.edu>
2024-05-12 17:49:42 -0500

    t

--- code/adna/flip_overlapping_snps.R
+++ code/adna/flip_overlapping_snps.R
@@ -15,26 +15,23 @@ suppressWarnings(suppressMessages({
 ukbb_file_wbs = args[1]
 ukbb_file_all = args[2]
 sds_file = args[3]
-outfile_r = args[4]
+outpath = args[4]
 
 # Read in UKBB freq file
 ukbb_wbs <- fread(ukbb_file_wbs)
-ukbb_wbs <- ukbb_wbs %>% separate(ID, c("chr", "POS"), remove = FALSE) %>% filter(ukbb_wbs$ALT_FREQS > 0.01 & ukbb_wbs$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
+ukbb_wbs <- ukbb_wbs %>% separate(ID, c("tmp", "POS"), remove = FALSE) %>% filter(ukbb_wbs$ALT_FREQS > 0.01 & ukbb_wbs$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
 ukbb_all <- fread(ukbb_file_all)
-ukbb_all <- ukbb_all %>% separate(ID, c("chr", "POS"), remove = FALSE) %>% filter(ukbb_all$ALT_FREQS > 0.01 & ukbb_all$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
+ukbb_all <- ukbb_all %>% separate(ID, c("tmp", "POS"), remove = FALSE) %>% filter(ukbb_all$ALT_FREQS > 0.01 & ukbb_all$ALT_FREQS < 0.99) %>% select("#CHROM",	"ID","POS","REF",	"ALT")
 ukbb <- full_join(ukbb_wbs, ukbb_all)
 
-# Read in SDS
+# Read in contrasts
 sds <- fread(sds_file)
 sds$POS <- as.character(sds$POS)
 
 # Join files by chromosome and position
-df <- inner_join(ukbb, sds, by = c("#CHROM" = "CHR", "POS" = "POS"))
+df <- inner_join(ukbb, sds, by = c("#CHROM" = "CHR", "POS" = "POS", "REF" = "REF", "ALT" = "ALT"))
 
-# Get rid of rows where any alleles don't match
-df_filter <- df %>% filter((DA == REF & AA == ALT) | (DA == ALT | AA == REF))
-
-# If derived and alternate allele don't match, flip SDS
+# Change allele frequency to alternate allele freq by taking 1 -
 df_flipped <- df_filter %>% mutate(SDS = case_when(DA == REF ~ (-1 * SDS), DA == ALT ~ SDS))
 
 # Select correct columns
