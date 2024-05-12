f090b6a23884c00937297870ef728284056b20d4
jgblanc <jgblanc@uchicago.edu>
2024-05-12 18:10:36 -0500

    t

--- code/adna/flip_overlapping_snps.R
+++ code/adna/flip_overlapping_snps.R
@@ -16,6 +16,7 @@ ukbb_file_wbs = args[1]
 ukbb_file_all = args[2]
 sds_file = args[3]
 outpath = args[4]
+chr_num = args[5]
 
 # Read in UKBB freq file
 ukbb_wbs <- fread(ukbb_file_wbs)
@@ -32,13 +33,21 @@ sds$POS <- as.character(sds$POS)
 df <- inner_join(ukbb, sds, by = c("#CHROM" = "CHR", "POS" = "POS", "REF" = "REF", "ALT" = "ALT"))
 
 # Change allele frequency to alternate allele freq by taking 1 -
-df_flipped <- df_filter %>% mutate(SDS = case_when(DA == REF ~ (-1 * SDS), DA == ALT ~ SDS))
+df_flipped <- df %>% mutate(EUROPEAN_NEOLITHIC = 1 - EUROPEAN_NEOLITHIC, BRONZE_AGE = 1 - BRONZE_AGE, HISTORICAL = 1 - HISTORICAL)
 
-# Select correct columns
-df_out <- df_flipped %>% select("#CHROM", "ID.x", "REF", "ALT", "SDS")
-colnames(df_out) <- c("#CHROM", "ID", "REF", "ALT", "r")
+# Select correct columns and save output
 
-# Save output
-fwrite(df_out,outfile_r, row.names = F, col.names = T, quote = F, sep = "\t")
+df_EN <- df_flipped %>% select("#CHROM", "ID.x", "REF", "ALT", "EUROPEAN_NEOLITHIC")
+colnames(df_EN) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_EN, paste0(outpath,"EUROPEAN_NEOLITHIC_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+
+df_BA <- df_flipped %>% select("#CHROM", "ID.x", "REF", "ALT", "BRONZE_AGE")
+colnames(df_BA) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_BA, paste0(outpath,"BRONZE_AGE_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+
+df_H <- df_flipped %>% select("#CHROM", "ID.x", "REF", "ALT", "HISTORICAL")
+colnames(df_H) <- c("#CHROM", "ID", "REF", "ALT", "r")
+fwrite(df_H, paste0(outpath,"HISTORICAL_chr", chr_num, ".rvec"), row.names = F, col.names = T, quote = F, sep = "\t")
+
 
 
