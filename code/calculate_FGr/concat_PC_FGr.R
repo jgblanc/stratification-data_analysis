fcb37945997e5306a1b9ae4b0befd5d762786013
jgblanc <jgblanc@uchicago.edu>
2024-05-13 15:05:14 -0500

    t

---
+++ code/calculate_FGr/concat_PC_FGr.R
@@ -0,0 +1,54 @@
+# This script concatenates results from computing the error in FGr
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript concat_results.R <output file> <input files>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(tidyverse)
+}))
+
+
+outfile = args[1]
+
+dfOut <- matrix(NA, nrow = 1, ncol = 10)
+
+for (i in 2:length(args)) {
+
+  # Get results file name
+  filename = args[i]
+
+  # Extract parameters
+  tmp <- strsplit(filename, "/")[[1]]
+  test <- tmp[4]
+  gwas <- tmp[5]
+  contrast <- strsplit(tmp[8], "_PCs_FGr.txt")[[1]][1]
+  chrtype_gwas <- tmp[6]
+  chrtype_test <- tmp[7]
+
+  # Read in results
+  dfTmp <- fread(filename)
+  dfTmp$test <- test
+  dfTmp$gwas <- gwas
+  dfTmp$contrast <- constrast
+  dfTmp$chrtype_gwas <- chrtype_gwas
+  dfTmp$chrtype_test <- chrtype_test
+
+  # Save results in table
+  dfOut <- rbind(dfOut, dfTmp)
+
+}
+
+# Remove first row
+dfOut <- as.data.frame(dfOut[2:nrow(dfOut),])
+colnames(dfOut) <- c("PC",	"r2",	"rho", 	"lc", "up", "subdataset", "gwas", "constrast", "chrtype_gwas", "chrtype_test")
+
+# Save file
+print(dfOut)
+fwrite(dfOut,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+
+
+
+
