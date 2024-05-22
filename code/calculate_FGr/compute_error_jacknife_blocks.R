6eeede8f85ab6d9d8611899e13d4faa5a8a296bb
jgblanc <jgblanc@uchicago.edu>
2024-05-22 12:26:10 -0500

    t

--- code/calculate_FGr/compute_error_jacknife_blocks.R
+++ code/calculate_FGr/compute_error_jacknife_blocks.R
@@ -22,7 +22,7 @@ data <- data[,3:ncol(data)]
 
 for (i in 2:22) {
 
-  print(paste0("chr num ",i)) 
+  print(paste0("chr num ",i))
   # Read in new chromosome
   filename <- paste0(Tm_prefix,"_", i, ".txt")
   tmp <- fread(filename)
@@ -54,19 +54,31 @@ Fbar <- apply(gammas, 1, mean)
 sigmas2 <- rep(0, nrow(data))
 for (i in 1:nrow(data)) {
 
-  sigmas2[i] <- (21/22) * sum((gammas[i,] - Fbar[i])^2)
+  sigmas2[i] <- ((nblocks - 1)/nblocks) * sum((gammas[i,] - Fbar[i])^2)
 
 }
 print(mean(sigmas2, na.rm = TRUE))
 
 FGr_hat <- apply(data, 1, mean)
-print(var(FGr_hat, na.rm = TRUE))
+
+# Get test stat
+f <- mean(FGr_hat)
+f2 <- f^2
+
+# Get variance
+jkVar <- mean(sigmas2, na.rm = TRUE)
+
+# Test for significance
+pval <- pchisq(f2, df =1, lower.tail = F)
 
 # Find proportion
-error <- mean(sigmas2, na.rm = TRUE) / var(FGr_hat, na.rm = TRUE)
+varFGr <- var(FGr_hat, na.rm = TRUE)
+error <- f / varFGr
 
 # Make output table
-dfOut <- as.data.frame(error)
+dfOut <- as.data.frame(matrix(NA, nrow = 1, ncol = 6))
+colnames(dfOut) <- c("f", "f2", "jkVar", "pval", "varFGr", "error")
+dfOut[1,] <- c(f,f2,jkVar,pval,varFGr,error)
 fwrite(dfOut, outfile, row.names = F, col.names = T, quote = F, sep = "\t")
 
 
