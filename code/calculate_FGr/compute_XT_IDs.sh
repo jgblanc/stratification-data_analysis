a3d855b5f1bcdc8d9911f07b8f83ca0ca211669e
jgblanc <jgblanc@uchicago.edu>
2024-06-18 13:44:34 -0500

    t

--- code/calculate_FGr/compute_XT_IDs.sh
+++ code/calculate_FGr/compute_XT_IDs.sh
@@ -4,16 +4,12 @@ pheno_path=$2
 test_type=$3
 outfile=$4
 overlap_snps=$5
-ids=$6
 
 plink2 \
   --pfile $pfile_path \
   --extract $overlap_snps \
   --glm omit-ref allow-no-covars \
-  --pheno  $pheno_path \
+  --pheno $pheno_path \
   --pheno-name $test_type \
   --geno-counts \
-  --keep $ids \
-  --memory 100000 \
-  --threads 16 \
   --out $outfile
