a410df73f0d0d24aa661fc9e4ffb962a0e9047e0
Jennifer Blanc <jgblanc@uchicago.edu>
2024-06-18 13:24:18 -0500

    t

---
+++ code/PGA_paper/compute_cord_r.R
@@ -0,0 +1,82 @@
+## Project Tvec per chromosome
+## This script projects the test vector from the test to the gwas panel using genotypes for a single chromosome
+
+args=commandArgs(TRUE)
+
+if(length(args)<6){stop("Rscript project_Tvec_chr.R <test panel prefix> <gwas panel prefix> <test vec file> <outfile>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+test_prefix = args[1]
+tvec_file_lat = args[2]
+tvec_file_long=args[3]
+out_prefix = args[4]
+overlap_snps = args[5]
+outfile_lat = args[6]
+outfile_long = args[7]
+
+####################
+## Functions #######
+####################
+
+# Compute G %*% t(X) %*% T
+compute_b <- function(path_to_test, testvec_file, test_type, outpath) {
+
+  # Compute t(X)T
+  outfile_XT <- paste0(outpath, "xt_temp")
+  pheno_file <- testvec_file
+  cmd_XT <- paste("sh code/calculate_FGr/compute_XT.sh", path_to_test, pheno_file, test_type, outfile_XT, overlap_snps, sep = " ")
+  system(cmd_XT)
+
+  # Adjust Betas to account for variance in x
+
+  # Read in betas and genotype counts
+  beta_plink <- fread(paste0(outpath, "xt_temp." , test_type ,".glm.linear"))
+  count_plink <- fread(paste0(outpath, "xt_temp.gcount"))
+
+  # Calculate length of mean centered genotypes from counts
+  nOBS <- (count_plink$HOM_REF_CT + count_plink$HET_REF_ALT_CTS + count_plink$TWO_ALT_GENO_CTS)
+  counts <- (count_plink$HOM_REF_CT * 0) + (count_plink$HET_REF_ALT_CTS * 1) + (count_plink$TWO_ALT_GENO_CTS * 2)
+  mean_gc <- counts / nOBS
+  length_mc_genos <- (count_plink$HOM_REF_CT * (-1 * mean_gc)^2) + (count_plink$HET_REF_ALT_CTS * (1 - mean_gc)^2) +  (count_plink$TWO_ALT_GENO_CTS * (2 - mean_gc)^2)
+
+  # Fix betas
+  betas_plink_norm <- beta_plink$BETA * length_mc_genos
+
+  #  Re-write .linear file with correct betas
+  beta_plink$BETA <- betas_plink_norm
+  beta_reformat <- beta_plink %>% dplyr::select("#CHROM","ID", "REF", "ALT",  "BETA")
+  beta_reformat[is.na(beta_reformat)] <- 0
+
+  return(beta_reformat)
+}
+
+
+#####################
+##     Main       ###
+#####################
+
+
+# Gather parameters
+testID <- fread(paste0(test_prefix, ".psam"))
+head(testID)
+colnames(testID) <- c("IID",  "Sex")
+n <- nrow(testID)
+
+# Compute b for latitude
+r_lat = compute_b(path_to_test = test_prefix, testvec_file = tvec_file_lat, test_type = "latitude", outpath = out_prefix)
+r_lat$BETA  <- r_lat$BETA * (1/n)
+colnames(r_lat)[5] <- "r"
+print(head(r_lat))
+fwrite(r_lat, outfile_lat, row.names = F, col.names = T, quote = F, sep = "\t")
+
+# Compute b for latitude
+r_long = compute_b(path_to_test = test_prefix, testvec_file = tvec_file_long, test_type = "longitude", outpath = out_prefix)
+r_long$BETA  <- r_long$BETA * (1/n)
+colnames(r_long)[5] <- "r"
+print(head(r_long))
+fwrite(r_long, outfile_long, row.names = F, col.names = T, quote = F, sep = "\t")
+
---
+++ code/PGA_paper/get_LatLong_Tvec.R
@@ -0,0 +1,36 @@
+## Get Lat/Long Test Vector
+## This script uses HGDP metadata and a psam to format test vectors for latitide and longitude
+
+args=commandArgs(TRUE)
+
+if(length(args)<4){stop("Rscript get_LatLong_Tvec.R <metadata> <psam> <outfile Lat> <outfile Long>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+md_file = args[1]
+psam_file = args[2]
+outfile_lat = args[3]
+outfile_long = args[4]
+
+# Read in metadata
+dfMeta <- fread(md_file)
+
+# Read in Psam
+dfPSam <- fread(psam_file)
+
+# Combine files
+df <- inner_join(dfPSam, dfMeta, by = c("#IID" = "sample"))
+
+# Only keep Eurasian samples
+df <- filter(region %in% c("CENTRAL_SOUTH_ASIA", "EUROPE",  "MIDDLE_EAST",  "EAST_ASIA"))
+
+# Set up outfiles
+dfLat <- df %>% select("#IID", "latitude")
+dfLong <- df %>% select("#IID", "longitude")
+
+# Save output
+fwrite(dfLat,outfile_lat, row.names = F, col.names = T, quote = F, sep = "\t")
+fwrite(dfLong,outfile_long, row.names = F, col.names = T, quote = F, sep = "\t")
