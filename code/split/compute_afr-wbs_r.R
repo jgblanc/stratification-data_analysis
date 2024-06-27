5af75c746f162a037eb0760b79d1e7c6a6f671b8
Jennifer Blanc <jgblanc@uchicago.edu>
2024-06-27 13:56:47 -0500

    t

---
+++ code/split/compute_afr-wbs_r.R
@@ -0,0 +1,28 @@
+# This script divides the WBS into GWAS an test panels
+
+args=commandArgs(TRUE)
+
+if(length(args)<3){stop("Rscript compute_afr-wbs.R")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+afr_file = args[1]
+wbs_file  = args[2]
+outfile = args[3]
+
+
+## Read in freq files
+dfAFR <- fread(afr_file)
+dfWBS <- fread(wbs_file)
+
+# Make new file
+dfOut <- dfAFR %>% select("#CHROM", "ID", "REF", "ALT")
+dfOut$r <- dfAFR$ALT_FREQS - dWBS$ALT_FREQS
+
+## Save
+fwrite(dfOut, outfile ,row.names=F,quote=F,sep="\t", col.names = T)
+
+
